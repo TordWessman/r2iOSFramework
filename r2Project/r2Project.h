cca33aee97d9e772d2199e4fe2a4423c9c08f441
Tord Wessman <tord@axelit.se>
2018-01-22 08:00:58 +0100

    Initial Commit

---
+++ r2Project/r2Project.h
@@ -0,0 +1,19 @@
+//
+//  r2Project.h
+//  r2Project
+//
+//  Created by Tord Wessman on 2018-01-22.
+//  Copyright © 2018 Axel IT AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for r2Project.
+FOUNDATION_EXPORT double r2ProjectVersionNumber;
+
+//! Project version string for r2Project.
+FOUNDATION_EXPORT const unsigned char r2ProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <r2Project/PublicHeader.h>
+
+

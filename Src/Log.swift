a930965d7984d6c88e2929c376c08a356e46e4d6
Tord Wessman <tord@axelit.se>
2018-03-05 15:15:47 +0100

    Add log level to Log

--- Src/Log.swift
+++ Src/Log.swift
@@ -8,11 +8,22 @@
 
 import Foundation
 
+public enum R2LogLevel: Int {
+    
+    case info = 0
+    case debug = 1
+    case warn = 2
+    case error = 3
+    
+}
+/** Simple interface for print log output */
 public class Log {
     
-    public class func d(_ what: Any?) {
+    /** Print to console. */
+    public class func d(_ what: Any?, _ level: LogLevel.debug) {
+        //TODO: implement log level.
         if let v = what {
-            print(v);
+            print("[\(String(describing: level))] \(String(describing: v))");
         } else {
             print ("nil")
         }
